379baad717580be4663853730726e3e1dc47b462
Steven Ortiz <sortiz1726@users.noreply.github.com>
2021-09-29 18:27:29 +0000

    adding solutions

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,41 @@
-function deafGrandma() {
-
+function isAnyLowerCase(string)
+{
+   return string.search(/[a-z]/) !== -1;
 }
 
-deafGrandma();
+console.log("HEY KID!");
+
+const prompt = require('prompt-sync')();
+
+let goodbyeCount = 0;
+
+while(goodbyeCount != 2)
+{
+    const response = prompt();
+
+    if(response === "")
+    {
+        console.log("WHAT!");
+    }
+    else if(isAnyLowerCase(response))
+    {
+        console.log("SPEAK UP, KID!");
+    }
+    else if(response === "GOODBYE!")
+    {
+        goodbyeCount++;
+        
+        if(goodbyeCount === 1)
+        {
+            console.log("LEAVING SO SOON?");
+        }
+        else if(goodbyeCount === 2)
+        {
+            console.log("LATER, SKATER!");
+        }
+    }
+    else
+    {
+        console.log("NO, NOT SINCE 1946!");
+    }
+}
--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,30 @@
-def deaf_grandma():
+def is_any_lowercase(word):
+    return any([letter for letter in word if letter.islower()])
 
+def deaf_grandma():
+    print("HEY KID!");
+
+    goodbye_count = 0;
+
+    while goodbye_count != 2:
+        
+        RESPONSE = input();
+
+        if RESPONSE == "":
+            print("WHAT!")
+        
+        elif is_any_lowercase(RESPONSE):
+            print("SPEAK UP, KID!")
+        
+        elif RESPONSE == "GOODBYE!":
+            goodbye_count += 1
+        
+            if goodbye_count == 1:
+                print("LEAVING SO SOON?")
+            elif goodbye_count == 2:
+                print("LATER, SKATER!")
+        else:
+            print("NO, NOT SINCE 1946!")
 
 
 deaf_grandma()
