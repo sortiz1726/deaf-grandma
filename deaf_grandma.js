15341c7b2ad581cc6a27f1b881b775dbad8963ed
Steven Ortiz <sortiz1726@users.noreply.github.com>
2021-10-04 19:44:40 +0000

    updating

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -3,39 +3,66 @@ function isAnyLowerCase(string)
    return string.search(/[a-z]/) !== -1;
 }
 
-console.log("HEY KID!");
-
-const prompt = require('prompt-sync')();
-
-let goodbyeCount = 0;
-
-while(goodbyeCount != 2)
+async function deafGrandma()
 {
-    const response = prompt();
+    console.log("HEY KID!");
 
-    if(response === "")
+    //let prompt = require('prompt-sync')();
+    const prompt = require('prompt');
+
+    let goodbyeCount = 0;
+
+    while(goodbyeCount != 2)
     {
-        console.log("WHAT!");
-    }
-    else if(isAnyLowerCase(response))
-    {
-        console.log("SPEAK UP, KID!");
-    }
-    else if(response === "GOODBYE!")
-    {
-        goodbyeCount++;
-        
-        if(goodbyeCount === 1)
+        //const response = prompt();
+        const {response} = await prompt.get(response);
+    
+        if(response === "")
         {
-            console.log("LEAVING SO SOON?");
+            console.log("WHAT!");
         }
-        else if(goodbyeCount === 2)
+        else if(isAnyLowerCase(response))
         {
-            console.log("LATER, SKATER!");
+            console.log("SPEAK UP, KID!");
         }
-    }
-    else
-    {
-        console.log("NO, NOT SINCE 1946!");
-    }
+        else if(response === "GOODBYE!")
+        {
+            goodbyeCount++;
+            
+            if(goodbyeCount === 1)
+            {
+                console.log("LEAVING SO SOON?");
+            }
+            else if(goodbyeCount === 2)
+            {
+                console.log("LATER, SKATER!");
+            }
+        }
+        else
+        {
+            console.log("NO, NOT SINCE 1946!");
+        }
+    } 
+}
+
+//deafGrandma();
+
+const prompt = require("prompt-async");
+ 
+async function example_async() // Available only with `prompt-async`!
+{
+    // Start the prompt.
+    prompt.start();
+ 
+    // Get two properties from the user: the `username` and `email`.
+    const {username, email} = await prompt.get(["username", "email"]);
+    // Get two properties from the user: the `password` and `food`.
+    const {password, food} = await prompt.get(["password", "food"]);
+ 
+    // Log the results.
+    console.log("Command-line input received: ");
+    console.log(`  username: ${username},`);
+    console.log(`  password: ${password},`);
+    console.log(`  email: ${email},`);
+    console.log(`  food: ${food}.`);
 }
